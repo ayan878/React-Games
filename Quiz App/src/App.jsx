5f4d1d7059aace63e69e6919543cb6af6269273f
ayan878 <ayanraza197@gmail.com>
2024-02-25 18:25:37 +0530

    tic-tac-toe

--- Quiz App/src/App.jsx
+++ Quiz App/src/App.jsx
@@ -1,4 +1,15 @@
+import Header from './components/Header.jsx';
+import Quiz from './components/Quiz.jsx';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
 }
+
 export default App;
