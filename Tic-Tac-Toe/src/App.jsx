a59b5542033844df14e167e3afcffe8284273619
ayan878 <ayanraza197@gmail.com>
2024-02-25 11:59:05 +0530

    Quiz-App

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -1,9 +1,24 @@
+import GameBoard from "./GameBoard";
 
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <main>
+      <div className="max-sm:">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+      </div>
+      <GameBoard/>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ Tic-Tac-Toe/src/GameBoard.jsx
@@ -0,0 +1,11 @@
+
+
+function GameBoard() {
+  return (
+    <div className="border-double border-4 border-indigo-600">
+      GameBoard
+    </div>
+  );
+}
+
+export default GameBoard
