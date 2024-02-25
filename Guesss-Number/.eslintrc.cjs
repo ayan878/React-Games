a5629c9aeb7ec50895ec8d297852f15ca704df43
ayan878 <ayanraza197@gmail.com>
2024-02-26 02:35:21 +0530

    state Management

--- Guesss-Number/.eslintrc.cjs
+++ Guesss-Number/.eslintrc.cjs
@@ -1,21 +1,31 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: {
+    ecmaVersion: 2022, // Use the specific ECMAScript version (latest is not a valid value)
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect", // Use 'detect' to automatically detect React version
+    },
+  },
+  plugins: ["react-refresh"],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
